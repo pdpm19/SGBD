6802bdac38f489bfe739235af7b451e2881e6005
pdpm19 <48568896+pdpm19@users.noreply.github.com>
2020-10-22 19:04:25 +0100

    Updated Edit App

---
+++ gui/connectDlg.py
@@ -0,0 +1,69 @@
+# Connect da aplicação || Login
+import sys
+import os
+import pyodbc
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+# Conectar à BD
+class Connect(QDialog):
+    def __init__(self, width, heigth, windowTitle, uiPath):
+        super(Connect, self).__init__()
+        iconPath = os.path.join(uiPath, 'images', 'logo.jpg')
+        self.setWindowIcon(QIcon(iconPath))  # Insere o icon
+        self.setGeometry(400, 200, width, heigth)
+        self.setWindowTitle(windowTitle)
+        
+        self.mainLayout = QVBoxLayout()
+        
+        # Layout em questionário
+        self.formWidget = QWidget()
+        self.formLayout = QFormLayout()
+        self.hostNameField = QLineEdit()
+        self.dbNameField = QLineEdit()
+        self.usernameField = QLineEdit()
+        self.userPasswordField = QLineEdit()
+        self.userPasswordField.setEchoMode(QLineEdit.Password)  # mostra ** ou invés de caracteres
+       
+        self.formLayout.addRow(QLabel('Host Name:'), self.hostNameField)
+        self.formLayout.addRow(QLabel('Database Name:'), self.dbNameField)
+        self.formLayout.addRow(QLabel('Username:'), self.usernameField)
+        self.formLayout.addRow(QLabel('Password:'), self.userPasswordField)
+        self.formWidget.setLayout(self.formLayout)
+        
+        # Botões de OK/Cancel
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.userPasswordField.editingFinished.connect(self.Accept)
+        self.buttonBox.rejected.connect(self.Back)
+        
+        
+        self.mainLayout.addWidget(self.formWidget)
+        self.mainLayout.addWidget(self.buttonBox)
+
+        # Atribuíção do layout principal
+        self.setLayout(self.mainLayout)
+
+    def Back(self):
+        sys.exit()
+
+    # Chamada à função que liga à BD
+    def Accept(self):
+        # Conectar à BD
+        self.buttonBox.accepted.connect(self.accept)
+
+        '''
+        # Debug à passagem dos dados 
+        print("""====================== BD ======================  
+    IP      : %s
+    Hostname: %s
+    Username: %s
+    Password: %s
+    ====================== BD ======================""" %(self.hostNameField.text(), self.dbNameField.text(), self.usernameField.text(), self.userPasswordField.text()))
+        '''   
+        
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+self.hostNameField.text()+';DATABASE='+self.dbNameField.text()+';UID='+self.usernameField.text()+';PWD='+self.userPasswordField.text(), autocommit=False)
+        ret = [self.dbNameField.text(), conn.cursor()]
+        return ret
+        
--- gui/gui.py
+++ gui/gui.py
@@ -8,14 +8,36 @@ from PyQt5.QtGui import *
 
 from connectDlg import Connect
 
+# Torna input em Table Mode View
+class TableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+    
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
+
 # Aplicação principal-
 class GUI(QMainWindow):
-    def __init__(self, windowTitle, uiPath):
+    def __init__(self, windowTitle, uiPath, cursor, dbName):
         super(GUI, self).__init__()
         iconPath = os.path.join(uiPath, 'images', 'logo.jpg')
         self.setWindowIcon(QIcon(iconPath))
         self.setGeometry(400, 200, 600, 400)
         self.setWindowTitle(windowTitle)
+        self.dbName = dbName
+        self.cursor = cursor
         
         # Vai conter 4 faces principais
         #   1. Página principal
@@ -69,9 +91,16 @@ class GUI(QMainWindow):
         appsWidget = QWidget()
         
         app1Button = QPushButton('App1 - Editar')
+        app1Button.pressed.connect(self.DisplayEdit)
+        
         app2Button = QPushButton('App2 - Browser')
+        app2Button.pressed.connect(lambda: self.stacked.setCurrentIndex(2))
+
         app3Button = QPushButton('App3 - Log tempo')
+        app3Button.pressed.connect(lambda: self.stacked.setCurrentIndex(3))
+
         app4Button = QPushButton('App4 - Log')
+        app4Button.pressed.connect(lambda: self.stacked.setCurrentIndex(4))
 
         appsLayout.addWidget(app1Button, 0,0)
         appsLayout.addWidget(app2Button, 0,1)
@@ -84,7 +113,7 @@ class GUI(QMainWindow):
         isolationWidget = QWidget()
         
         self.isolationComboBox = QComboBox()
-        self.isolationComboBox.addItems(['Read Uncommited', 'Read Committed', 'Repeatable Read', 'Serializable'])
+        self.isolationComboBox.addItems(['Read Uncommitted', 'Read Committed', 'Repeatable Read', 'Serializable'])
         
         isolationLayout.addRow(QLabel('Nível de isolamento:'), self.isolationComboBox)
         isolationWidget.setLayout(isolationLayout)
@@ -95,16 +124,154 @@ class GUI(QMainWindow):
     
     # Vai permitir ao utilizador Editar as encomendas
     def EditUI(self):
-        print('2')
+        backButton = QPushButton('Voltar')
+        acceptButton = QPushButton('Aceitar')
+        
+        buttonsLayout = QHBoxLayout()
+        buttonsWidget = QWidget()
+
+        buttonsLayout.addWidget(backButton)
+        buttonsLayout.addWidget(acceptButton)
+        buttonsWidget.setLayout(buttonsLayout)
+        
+        backButton.pressed.connect(self.CloseEdit)
+        acceptButton.pressed.connect(self.EditFunction)
+        
+        formLayout = QFormLayout()
+        formWidget = QWidget()
+
+        self.encIDField = QComboBox()
+        self.encIDs = []
+        self.clientIDField = QLineEdit()
+        self.clientIDs = []
+        self.clientNameField = QLineEdit()
+        self.clientName = []
+        self.clientAddressField = QLineEdit()
+        self.clientAddress = []
+        
+        formLayout.addRow(QLabel('ID da Encomenda:'), self.encIDField)
+        formLayout.addRow(QLabel('ID do Cliente:'), self.clientIDField)
+        formLayout.addRow(QLabel('Nome do Cliente:'), self.clientNameField)
+        formLayout.addRow(QLabel('Morada do Cliente:'), self.clientAddressField) 
+        formWidget.setLayout(formLayout)       
+        
+        self.editLayout.addWidget(formWidget)
+        self.editLayout.addWidget(buttonsWidget)
+        self.editWidgets.setLayout(self.editLayout)
+    
+    # Carrega a página de Editar & preenche os campos
+    def DisplayEdit(self):
+        self.stacked.setCurrentIndex(1)
+        # Faz o pedido à BD 
+        query = MakeTransaction(self.cursor, self.isolationComboBox.currentText(), 'SELECT', ['*'], self.dbName, 'Encomenda', [])
+        
+        for row in query:
+            self.encIDs.append(str(row[0]))
+            self.clientIDs.append(str(row[1]))
+            self.clientName.append(str(row[2]))
+            self.clientAddress.append(str(row[3]))
+        
+
+        self.encIDField.addItems(self.encIDs)
+        self.clientNameField.setText(self.clientName[0])
+        self.clientIDField.setText(self.clientIDs[0])
+        self.clientAddressField.setText(self.clientAddress[0])
+        
+        self.encIDField.currentIndexChanged.connect(self.UpdateEdit)
+    
+    # Fecha a página de Editar
+    def CloseEdit(self):
+        # Vai limpar todos os campos
+        self.encIDField.clear()
+        self.clientIDField.clear()
+        self.clientNameField.clear()
+        self.clientAddressField.clear()
+
+        # vai limpar todos os vetores auxiliares
+        self.encIDs.clear()
+        self.clientIDs.clear()
+        self.clientName.clear()
+        self.clientAddress.clear()
+
+        self.stacked.setCurrentIndex(0)
+    
+    # Atualiza os campos consoante o ID da encomenda selecionado
+    def UpdateEdit(self):
+        currentID = self.encIDField.currentIndex()
+        
+        self.clientNameField.clear()
+        self.clientNameField.setText(self.clientName[currentID])
+
+        self.clientIDField.clear()
+        self.clientIDField.setText(self.clientIDs[currentID])
+
+        self.clientAddressField.clear()
+        self.clientAddressField.setText(self.clientAddress[currentID])
+    
+    # Grava as alterações feitas
+    def EditFunction(self):
+        print('yo')
     
     def BrowserUI(self):
-        print('3')
-    
+        backButton = QPushButton('Voltar')
+        backButton.pressed.connect(lambda: self.stacked.setCurrentIndex(0))
+
+        self.browserLayout.addWidget(backButton)
+        self.browserWidgets.setLayout(self.browserLayout)
+
     def TimeLogUI(self):
-        print('4')
+        backButton = QPushButton('Voltar')
+        backButton.pressed.connect(lambda: self.stacked.setCurrentIndex(0))
+    
+        self.timeLogLayout.addWidget(backButton)
+        self.timeLogWidgets.setLayout(self.timeLogLayout)
     
     def LogUI(self):
-        print('5')
+        backButton = QPushButton('Voltar')
+        backButton.pressed.connect(lambda: self.stacked.setCurrentIndex(0))
+
+        self.logLayout.addWidget(backButton)
+        self.logWidgets.setLayout(self.logLayout)
+
+# As transações de leitura são feitas aqui
+def MakeTransaction(cursor, isolationLevel: str, operation : str, returnRow: list, dbName: str, tableName: str, restrictor: list):
+    sqlCommand = 'SET TRANSACTION ISOLATION LEVEL ' + isolationLevel.upper() + ';\n'
+    sqlCommand = sqlCommand + 'BEGIN TRANSACTION;\n'
+    
+    # SELECT, UPDATE or DELETE
+    if len(returnRow) == 1:
+        sqlCommand = sqlCommand + operation.upper() + ' ' + returnRow[0] + ' FROM '
+    else:
+        sqlCommand = sqlCommand + operation.upper() + ' '
+        for i in returnRow:
+            sqlCommand = sqlCommand + i + ', '
+        sqlCommand = sqlCommand[:len(sqlCommand-2)] + ' FROM '
+
+    
+    # Introduz o nome da table
+    sqlCommand = sqlCommand + dbName + '.dbo.' +  tableName
+    
+    # Não tem nada a restringir
+    if len(restrictor) == 0:
+        sqlCommand = sqlCommand + ';'
+    else:
+        sqlCommand = sqlCommand + 'WHERE '
+        for i in restrictor:
+            aux = i
+            # Última restrição
+            if len(aux) == 3:
+                sqlCommand = sqlCommand + aux[0] + aux[1] + aux[2] + ';'
+            else:
+                sqlCommand = sqlCommand + aux[0] + aux[1] + aux[2] + ' ' + aux[3] + ' '
+
+    
+    sqlCommand = sqlCommand + '\nCOMMIT TRANSACTION;'
+    
+    # Faz por fim a query à BD
+    print(sqlCommand)
+    query = cursor.execute(sqlCommand)
+    
+    return query
 
 def DisplayGUI(args):
     uiPath = args
@@ -123,7 +290,7 @@ def DisplayGUI(args):
         for row in cursor:
             print(row)
         '''
-        window = GUI('Aplicação', uiPath)
+        window = GUI('Aplicação', uiPath, cursor, dbName)
         window.show()
     sys.exit(app.exec_())
     
