4cbd79214e80e7b9d38d42a999dc941ac66b9cbf
pdpm19 <48568896+pdpm19@users.noreply.github.com>
2022-01-19 18:56:25 +0000

    Add files via upload
    Added script to install Python3.8, PIP and all needed packages on Linux

---
+++ script.sh
@@ -0,0 +1,12 @@
+# For install python3.8 plus the extras
+sudo apt install python3.8
+
+sudo apt install unixodbc-dev
+
+# Isntall PIP3
+sudo apt install python3-pip
+
+# install pyodbc, pyqt5 and numpy
+pip3 install pyodbc
+pip3 install pyqt5
+pip3 install numpy
